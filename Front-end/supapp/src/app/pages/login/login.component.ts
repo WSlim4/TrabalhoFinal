11b124bea80777ad5bd31443d73515ab445f8049
Arthur Chaitter <Arthur.chaitter47@gmail.com>
2019-02-12 16:14:20 -0200

    *correção login, +validações de login

--- Front-end/supapp/src/app/pages/login/login.component.ts
+++ Front-end/supapp/src/app/pages/login/login.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+	passwordError: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  passwordCheck(senha){
+  	if(senha.value.length < 6){
+  		this.passwordError = true;
+  	}else{
+  		this.passwordError = false;
+  	}
   }
 
 }
