6356edd5c68d31ea106f7542faaad7f8e512772f
bianca <biaguimaraens@gmail.com>
2019-02-11 22:34:26 -0200

    +home +sidenav

--- Front-end/supapp/src/app/app-routing.module.ts
+++ Front-end/supapp/src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+
 
 const routes: Routes = [
-	{ path: 'login', component: LoginComponent }
+	{ path: 'login', component: LoginComponent },
+	{ path: '', redirectTo: '/home', pathMatch: 'full'},
+	{ path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
