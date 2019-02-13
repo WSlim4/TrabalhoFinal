c5ed5c9a7ea13d371bb07cb6fff9a257bfe1e57a
bianca <biaguimaraens@gmail.com>
2019-02-13 18:22:58 -0200

    #botoes sidenav

--- Front-end/supapp/src/app/pages/home/home.component.ts
+++ Front-end/supapp/src/app/pages/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  clicked: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+  onClick(){
+    this.clicked=!this.clicked;
   }
 
 }
