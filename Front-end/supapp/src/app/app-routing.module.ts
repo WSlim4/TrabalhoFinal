7ed427cbdd7b139bcd791d47ad214f9d46dd7da3
bianca <biaguimaraens@gmail.com>
2019-02-18 10:52:10 -0300

    +rotas dos cadastros +layout base da cadastrese

--- Front-end/supapp/src/app/app-routing.module.ts
+++ Front-end/supapp/src/app/app-routing.module.ts
@@ -2,18 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
+import { CadastreseComponent } from './pages/cadastrese/cadastrese.component';
+import { CadastroClienteComponent } from './pages/cadastro-cliente/cadastro-cliente.component';
+import { CadastroEmpresaComponent } from './pages/cadastro-empresa/cadastro-empresa.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AreaPesquisaComponent } from './pages/area-pesquisa/area-pesquisa.component';
 import { SobrenosComponent } from './pages/sobrenos/sobrenos.component';
 import { ConfiguracoesComponent } from './pages/configuracoes/configuracoes.component';
 
 const routes: Routes = [
-	{ path: 'login', component: LoginComponent },
-	{ path: '', redirectTo: '/home', pathMatch: 'full'},
 	{ path: 'home', component: HomeComponent },
+	{ path: '', redirectTo: '/home', pathMatch: 'full'},
+	{ path: 'login', component: LoginComponent },
+	{ path: 'cadastro', component: CadastreseComponent },
+	{ path: 'cadastroCliente', component: CadastroClienteComponent },
+	{ path: 'cadastroEmpresa', component: CadastroEmpresaComponent },
 	{ path: 'area-pesquisa', component: AreaPesquisaComponent },
 	{ path: 'sobrenos', component: SobrenosComponent },
 	{ path: 'configuracoes', component: ConfiguracoesComponent },
+
 ];
 
 @NgModule({
