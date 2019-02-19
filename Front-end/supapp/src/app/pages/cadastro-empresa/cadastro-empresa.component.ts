8370014f82dabb56db54b7c3499bbced1db70b39
Arthur Chaitter <Arthur.chaitter47@gmail.com>
2019-02-19 13:36:59 -0300

    +CadastroEmpresa pronto

--- Front-end/supapp/src/app/pages/cadastro-empresa/cadastro-empresa.component.ts
+++ Front-end/supapp/src/app/pages/cadastro-empresa/cadastro-empresa.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroEmpresaComponent implements OnInit {
 
+  passwordError: boolean = false;
+  numberError: boolean = false;
+  cnpjError: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
@@ -14,6 +18,30 @@ export class CadastroEmpresaComponent implements OnInit {
 
   onSubmit(cadastroEmpresa){
     console.log(cadastroEmpresa)
+  }
+
+  checkPassword(senha){
+    if(senha.value.length < 6){
+      this.passwordError = true;
+    }else{
+      this.passwordError = false;
+    }
+  }
+
+  checkNumber(telefone){
+    if(telefone.value.length < 8){
+      this.numberError = true;
+    }else{
+      this.numberError = false;
+    }
+  }
+
+  checkCnpj(cnpj){
+    if( cnpj.value.length != 14 ){
+      this.cnpjError = true;
+    }else{
+      this.cnpjError = false;
+    }
   }
 
 }
